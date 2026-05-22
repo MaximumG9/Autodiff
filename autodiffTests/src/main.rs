582f7b94331177ad5d131f0062208037051c0c3a
maximumg9 <maximumg999@gmail.com>
2026-05-22 11:03:31 -0700

    Improve example

--- autodiffTests/src/main.rs
+++ autodiffTests/src/main.rs
@@ -4,20 +4,29 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use num::Float;
-    use autodiff::gfloat::GFloat;
     use autodiffmacros::grad;
 
-    fn test<F : Float>(a: F, b: F) -> F {
+    const EPSILON : f64 = 0.0001f64;
+    fn similar_f64(a : f64, b : f64) -> bool {
+        (a - b).abs() < EPSILON
+    }
+    fn similar_array<const WIDTH: usize>(a : [f64;WIDTH], b : [f64;WIDTH]) -> bool {
+        a.iter()
+            .zip(b)
+            .all(
+                |(a,b)|
+                    (a - b).abs() < EPSILON
+            )
+    }
+
+    fn paraboloid<F : Float>(a: F, b: F) -> F {
         a * a + b * b
     }
 
     #[test]
     fn simple_macros() {
-        let result = grad!(test(1f64,2f64));
-
-        assert_eq!(result, GFloat {
-            value: 10f64,
-            grad: [1f64,1f64]
-        });
+        let result = grad!(paraboloid(5f64,2f64));
+        assert!(similar_f64(result.value, 29f64));
+        assert!(similar_array(result.grad, [10f64, 4f64]));
     }
 }
